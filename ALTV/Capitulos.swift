7fa67a9091e433c4c91f3fbbc11b59abd23b7ce7
Alan Olivares <alanolivares@alans-imac.local>
2018-10-18 00:05:27 -0500

    kj

--- ALTV/Capitulos.swift
+++ ALTV/Capitulos.swift
@@ -7,23 +7,26 @@
 //
 
 import UIKit
-
-class Capitulos: UIViewController {
+import AVKit
+import AVFoundation
+class Capitulos: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var paso = [SerieObjeto]()
-    var va = 0
-    var cap = [String: AnyObject]()
+    var va = ""
+    var imageSe = ""
+    var cap = [String]()
     var link = [String]()
     @IBOutlet var imageSerie: UIImageView!
+    
+    @IBOutlet var tableViewCap: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let objeto = paso
-        let val = va
-        let url = NSURL(string: objeto[val].imagen)!
+        tableViewCap.delegate = self
+        tableViewCap.dataSource = self
+        let url = NSURL(string: imageSe)!
         let data = NSData(contentsOf: url as URL)
         self.imageSerie.image=UIImage(data: data! as Data)
-        
-        print(val)
-        
+        self.title = va
+        buscarCapitulos()
         // Do any additional setup after loading the view.
     }
 
@@ -31,6 +34,36 @@ class Capitulos: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func buscarCapitulos(){
+        for capi in paso {
+            if(capi.name == va){
+                cap.append(capi.capitulo)
+                link.append(capi.link)
+            }
+        }
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cap.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "custom4", for: indexPath) as! Capitulo
+        cell.verCapitulo.text = cap[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let videoURL = URL(string: link[indexPath.row])
+        let player = AVPlayer(url: videoURL!)
+        let playerViewController = LandscapeAVPlayerController()
+        playerViewController.player = player
+        self.present(playerViewController, animated: true) {
+            playerViewController.player!.play()
+        }
+        
+    }
+    
     
 
     /*
@@ -43,4 +76,8 @@ class Capitulos: UIViewController {
     }
     */
 
+}
+class Capitulo:UITableViewCell{
+    @IBOutlet var verCapitulo: UILabel!
+    
 }
